c0131f0767598b51101e5db4c418eb0922ea627b
sgragg5619 <sgragg5619@gmail.com>
2021-12-16 21:52:56 +0800

    Implement Delete a comment functionality

--- pingler-backend/graphql/resolvers/comments.js
+++ pingler-backend/graphql/resolvers/comments.js
@@ -1,6 +1,6 @@
 import Post from "../../models/Post.js";
 import checkAuth from "../../utils/check-auth.js";
-import { UserInputError } from "apollo-server";
+import { UserInputError, AuthenticationError } from "apollo-server";
 import log from "../../utils/log.js";
 const commentsResolver = {
   Mutation: {
@@ -15,7 +15,7 @@ const commentsResolver = {
 
     createComment: async (_, { postId, body }, context) => {
       const { username } = checkAuth(context);
-      log("createComment: start", { running: true, username });
+      log("createComment: start", { running: true, username, postId, body });
 
       if (body.trim() === "") {
         log("createComment: empty body", { success: false });
@@ -28,7 +28,7 @@ const commentsResolver = {
 
       const post = await Post.findById(postId);
       if (post) {
-        log("createComment: post found", { post: post._id });
+        log("createComment: post found, commenting", { post: post._id });
         post.comments.unshift({
           body,
           username,
@@ -38,6 +38,54 @@ const commentsResolver = {
         return post;
       } else {
         log("createComment: post not found", { success: false });
+        throw new UserInputError("Post not found");
+      }
+    },
+
+    /**
+     *
+     * @param {*} _ Parent resolvers
+     * @param {String} postId
+     * @param {String} commentId
+     * @param {*} context headers
+     * @description Delete a comment from the post
+     */
+
+    deleteComment: async (_, { postId, commentId }, context) => {
+      const { username } = checkAuth(context);
+      log("deleteComment: started", {
+        running: true,
+        username,
+        postId,
+        commentId,
+      });
+
+      const post = await Post.findById(postId);
+      if (post) {
+        log("deleteComment: post found", { postId: post.id });
+
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+        if (commentIndex !== -1) {
+          log("deleteComment: comment found, deleting comment", {
+            postId: post.id,
+            commentId,
+          });
+          if (post.comments[commentIndex].username === username) {
+            post.comments.splice(commentIndex, 1);
+            await post.save();
+            return post;
+          } else {
+            log("deleteComment: unable to delete others comment", {
+              success: false,
+            });
+            throw new AuthenticationError("Action not allowed");
+          }
+        } else {
+          log("deleteComment: comment not found", { success: false });
+          throw new UserInputError("Comment not found inside the post");
+        }
+      } else {
+        log("deleteComment: post not found", { success: false });
         throw new UserInputError("Post not found");
       }
     },
